5029e32bcf2cadcbef6e2aecf6adc5eb3459bd31
jorgevillmal <jorgevillmal@ciencias.unam.mx>
2022-05-31 04:31:51 -0500

    Se incorporo la funcion newAddress

--- Practica3/Practica3.hs
+++ Practica3/Practica3.hs
@@ -1,8 +1,64 @@
 module Practica3 where
 import Sintax
+import Data.List
 
 type Address = Int
 
 type Value = Expr
 type Cell = (Address, Value)
 type Memory = [Cell]
+
+{--
+ -- Dada una memoria, genera una nueva direccion de memoria
+ -- que no este contenida en esta.
+--}
+
+minFreeMemory :: [Address] -> Address
+minFreeMemory xs = head ([0..]\\ xs)
+
+repeatedMemory :: Eq a => [a] -> Bool
+repeatedMemory[] = True
+repeatedMemory [x] = True
+repeatedMemory(x:xs) | x `elem` xs = False
+                     | otherwise = repeatedMemory xs
+
+listMemory :: Memory -> [Address]
+listMemory[] = []
+listMemory[x]=[fst x]
+listMemory(x:xs)=[fst x] ++ listMemory xs
+
+
+newAddress :: Memory -> Expr
+newAddress xs | repeatedMemory(listMemory xs) = L (minFreeMemory (listMemory xs))
+              | otherwise = error "Corrupted memory."
+
+
+-- *****************   Test newAddress ***********************
+newAddress1 = newAddress [ ]
+newAddress2 = newAddress [ ( 0 , B False ) , ( 2 , I 9 ) ]
+newAddress3 = newAddress [ ( 0 , I 21 ) , ( 1 , Void) , ( 2 , I 12 ) ]
+newAddress4 = newAddress [ ( 0 , I 21 ) , ( 1 , Void) , ( 2 , I 12 ) , ( 1 , B True) ]
+
+
+{--
+ -- Dada una dreccion de memoria, devuelve el valor contenido en la celda con tal 
+ -- direccion, en caso de no encontrarla regresara Nothing
+ --}
+
+contains :: Eq a => a -> [a] -> Bool
+contains = \elem -> \myList ->
+  case myList of
+    [] -> False 
+    x:xs | x == elem -> True 
+    _:xs -> contains elem xs 
+
+
+access ::  Address -> Memory -> Maybe Value
+access a (x:xs) | eval1 (Dref (snd x)) = let e = access snd x
+
+
+
+-- *****************   Test access ***********************
+access1 = access 3 [ ]
+
+
--- Practica3/Sintax.hs
+++ Practica3/Sintax.hs
@@ -26,9 +26,53 @@ data Expr = Var Identifier | I Int | B Bool
           | Void
           | Seq Expr Expr
           | While Expr Expr
+          | Abs Identifier Expr
           deriving (Show, Eq)
+
+data Maybe a = Just a 
+             | Nothing
 
 type Substitution = (Identifier, Expr)
 {--
     Igual deben adecuar las funciones realizas en la practica 1 para ajustarse a esta sintaxis
 --}
+
+
+-- ****************** Semántica Dinámica ***********************
+
+eval1 :: Expr -> Expr
+eval1 (Var v) = (Var v)
+eval1 (I n) = (I n)
+eval1 (B b) = (B b)
+eval1 (Add (I n) (I m)) = (I (n+m))
+eval1 (Add (I n) t) = (Add (I n) (eval1(t)))
+eval1 (Add t1 t2) = (Add (eval1(t1)) t2)
+eval1 (Mul (I n) (I m)) = (I (n*m))
+eval1 (Mul (I n) t) = (Mul (I n) (eval1(t)))
+eval1 (Mul t1 t2) = (Mul (eval1(t1)) t2)
+eval1 (Not (I n)) = (I (-n))
+eval1 (Not t) = (Not (eval1(t)))
+eval1 (Succ (I n)) = (I (n+1))
+eval1 (Succ t) = (Succ (eval1(t)))
+eval1 (Pred (I n)) = (I (n-1))
+eval1 (Pred t) = (Pred (eval1(t)))
+eval1 (Not (B True)) = (B False)
+eval1 (Not (B False)) = (B True)
+eval1 (Not t) = (Not (eval1(t)))
+eval1 (And (B True) (B True)) = (B True)
+eval1 (And (B a) (B b)) = (B False)
+eval1 (And (B b) t) = (And (B b) (eval1(t)))
+eval1 (And t1 t2) = (And (eval1(t1)) t2)
+eval1 (Or (B False) (B False)) = (B False)
+eval1 (Or (B a) (B b)) = (B True)
+eval1 (Or (B b) t) = (Or (B b) (eval1(t)))
+eval1 (Or t1 t2) = (Or (eval1(t1)) t2)
+eval1 (If (B True) t2 t3) = t2
+eval1 (If (B False) t2 t3) = t3
+eval1 (If t1 t2 t3) = (If (eval1(t1)) t2 t3)
+eval1 (Iszero (I 0)) = (B True)
+eval1 (Iszero (I n)) = (B False)
+eval1 (Iszero t) = (Iszero (eval1(t)))
+eval1 (Let (B b) (Abs x y) (Abs e a)) = (subs e (x,(B b)))
+eval1 (Let (I n) (Abs x y) (Abs e a)) = (subs e (x,(I n)))
+eval1 (Let t1 t2 t3) = (Let (eval1(t1)) t2 t3)
