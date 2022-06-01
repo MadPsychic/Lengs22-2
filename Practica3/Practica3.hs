0f1bc349d5a84f6c784839d58cc5fb88b7024019
MadPsychic <jonas.garciac@ciencias.unam.mx>
2022-05-31 21:31:19 -0500

    falta let y assign

--- Practica3/Practica3.hs
+++ Practica3/Practica3.hs
@@ -71,7 +71,7 @@ access n xs = if distintos (listMemory xs)
   else if contains n (listMemory xs) then Just (Sintax.eval1 ((listValue xs) !!(n-1)))
        else Nothing
 
-eval1 :: (Memory , Expr) -> (Memory , Expr)
+eval1 :: (Memory, Expr) -> (Memory, Expr)
 eval1 (mem, Var x) = (mem, Var x)
 eval1 (mem, I x) = (mem, I x)
 eval1 (mem, B x) = (mem, B x)
@@ -91,7 +91,7 @@ eval1 (mem, (Or (B b) (B o))) = (mem, B (b || o))
 eval1 (mem, (Or e t)) = (mem, Or e t)
 eval1 (mem, If (B False) e t) = (mem, t)
 eval1 (mem, If (B True) e t) = (mem, e)
-eval1 (mem, If c e t) = (mem, (If nc e t))
+eval1 (mem, If c e t) = (memAux, (If nc e t))
   where (memAux, nc) = Practica3.eval1 (mem, c)
 eval1 (mem, Iszero (I e)) = if e == 0
                             then (mem, B True)
@@ -103,9 +103,19 @@ eval1 (mem, Gt (I n) (I m)) = (mem, B (n > m))
 eval1 (mem, Gt e t) = (mem, Gt e t)
 eval1 (mem, Eq (I n) (I m)) = (mem, B (n == m))
 eval1 (mem, Eq e t) = (mem, Eq e t)
+eval1 (mem, Seq Void e) = (mem, e)
+eval1 (mem, Seq t e) = (memAux, Seq (s) (e))
+  where (memAux, s) = Practica3.eval1 (mem, t)
+eval1 (mem, While e t) = (mem, If e (Seq t (While e t)) Void)
 
 -- *****************   Test access ***********************
 access1 = access 3 [ ]
 access2 = access 1 [ ( 0 , B False ) , ( 2 , I 9 ) ]
 access3 = access 2 [ ( 0 , I 21 ) , ( 2 , I 12 ) , ( 1 , Void) ]
 access4 = access 2 [ ( 0 , I 21 ) , ( 0 , B False ) , ( 3 , Void) , ( 2 , I 12 ) ]
+
+-- *****************   Test eval1 ***********************
+evalT1 = Practica3.eval1 ( [ ( 0 , B False ) ] , (Add ( I 1 ) ( I 2 ) ) )
+evalT2 = Practica3.eval1 ( [ ( 0 , B False ) ] , ( Let "x" ( I 1 ) (Add (Var "x" ) ( I 2 ) ) ) )
+evalT3 = Practica3.eval1 ( [ ( 0 , B False ) ] , Assign (L 0 ) (B True) )
+evalT4 = Practica3.eval1 ( [ ] , While (B True) (Add ( I 1 ) ( I 1 ) ) )
