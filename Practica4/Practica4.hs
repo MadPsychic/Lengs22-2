dbfc30fd813e63dd14c7d7943658d5d9b1045222
jorgevillmal <53312702+jorgevillmal@users.noreply.github.com>
2022-06-19 15:42:21 -0500

    Update Practica4.hs

--- Practica4/Practica4.hs
+++ Practica4/Practica4.hs
@@ -1,27 +1,48 @@
-module Practica3 where
+module Practica4 where 
 
 import Sintax
-import Data.List
-import Data.Maybe
+import Data.Bool
 
+-- Alias para direcciones de memoria.
 type Address = Int
+{-- Alias para valores. Aunque por implementacion se podria poner cualquier expresion, se espera solo
+sean valores. --}
 
 type Value = Expr
-type Cell = (Address, Value)
-type Memory = [Cell]
 
+type Cell = ( Address , Value )
+
+type Memory = [ Cell ]
+
+data State = E Stack Memory Expr
+           | R Stack Memory Expr
+           | P Stack Memory Expr
+
+instance Show State where
+    show e = case e of 
+        E x y z -> show x ++ show y ++ show z
+        R x y z -> show x ++ show y ++ show z
+        P x y z -> show x ++ show y ++ show z
+
+
+
+-- Definicion de marcos
 data Frame = AddFL Expr | AddFR Expr
             | MulFL Expr | MulFR Expr
             | SuccF | PredF
-            | AndFL Expr | AndFR Expr
+            | AndFL Expr | AndFR Expr 
             | OrFL Expr | OrFR Expr
             | NotF | IszeroF
             | LtFL Expr | LtFR Expr
-            | GtFL Expr | GtFR Expr
+            | GtFL Expr | GtFR Expr 
             | EqFL Expr | EqFR Expr
             | IfF Expr Expr
-            | LetF Identifier Expr
+            | LetM Identifier Expr
             | AppFL Expr | AppFR Expr
+            | AllocF
+            | DrefF
+            | AssignFL Expr | AssignFR Expr
+            | SeqF Expr
             deriving (Eq)
 
 instance Show Frame where
@@ -35,7 +56,7 @@ instance Show Frame where
         AndFL x -> "And( - , " ++ show x ++ " )"
         AndFR x -> "And( " ++ show x ++ " , - )"
         OrFL x -> "Or( - , " ++ show x ++ " )"
-        OrFR x -> "Or( " ++ show x ++ " , - )"
+        OrFR x -> "Or( " ++ show x ++ " , - )"       
         NotF -> "Not( - )"
         IszeroF -> "IsZero( - )"
         LtFL x -> "Lt( - , " ++ show x ++ " )"
@@ -48,101 +69,81 @@ instance Show Frame where
      -- TODO LetF id t -> ?
         AppFL x -> "App( - , " ++ show x ++ " )"
         AppFR x -> "App( " ++ show x ++ " , - )"
+        AllocF  -> "Alloc( - )"
+        DrefF  -> "Dref( - )"
+        AssignFL x -> "Assing( - , " ++ show x ++" )"
+        AssignFR x -> "Assing( " ++ show x ++" , - )"
+        SeqF x -> "Seq("++ show x ++ " )"
 
-data Stack = Empty | S Frame Stack
 
-data State = E Stack Memory Expr
-           | R Stack Memory Expr
+--Definicion de pila de marcos
+data Stack = Empty
+           | S Frame Stack
+
+instance Show Stack where 
+    show e = case e of 
+        Empty -> "[ ]"
+        S x y -> show x ++ show y
+
+
+
+{--
+eval1. Re implementa esta función para que dado un estado, devuelva un paso de transicion, 
+esdecir,eval1s=s’siysólosis→k s’
+--}
 
 eval1 :: State -> State
-eval1 = error "implementar"
+--- VALUES
+eval1 (E s m e@(I n)) = R s m e
+eval1 (E s m e@(B b)) = R s m e
+eval1 (E s m e@(Fn x exp)) = R s m e
 
--- *****************   Test eval1 ***********************
--- eval1T0 = eval1 (E Empty (Add (I 2) (I 3)) )
--- eval1T1 = eval1 E (S (AddFL (I 3)) Empty) (I 2)
+--- ADD
+eval1 (E s m (Add e1 e2)) = E (S (AddFL e2) s) m e1
+eval1 (R (S (AddFL e2) s) m v) = E (S (AddFR v) s) m e2
+eval1 (R (S (AddFR (I v1)) s) m (I v2)) = R s m (I (v1+v2))
 
-evals :: State -> State
-evals = error "implementar"
-
--- *****************   Test eval1 ***********************
--- evals0 = evals (E Empty (Let "x" ( I 2 ) (Mul (Add ( I 1 ) ( V "x" ) ) (V "x" ) ) ) )
--- evals1 = evals (E Empty (Let "x" (B True) (If (Var "x") (Var "x") (B False) ) ) )
-
-evale :: Expr -> Expr
-evale = error "implementar"
-
--- *****************   Test eval1 ***********************
-evale0 = evale (Add (Mul ( I 2 ) ( I 6 ) ) (B True) )
-evale1 = evale (Or (Eq (Add ( I 0 ) ( I 0 ) ) ( I 0 ) ) (Eq ( I 1 ) (I 10) ) )
-
--- ***************** Manejo de excepciones ***********************
-{-- TODO frVars. Extiende esta función para las nuevas expresiones.
-frVars :: Expr -> [ Identifier ]
-frVars (Var v) = [v]
-frVars (I x) = []
-frVars (B x)  = []
-frVars (L x ) = []
-frVars (Add x y) = (frVars x) ++ (frVars y)
-frVars (Mul x y) = (frVars x)++ (frVars y)
-frVars (Succ x) = (frVars x)
-frVars (Pred x) = (frVars x)
-frVars (And x y) = (frVars x)++ (frVars y)
-frVars (Or x y) = (frVars x) ++ (frVars y)
-frVars (Not x) = (frVars x)
-frVars (Iszero x) = (frVars x)
-frVars (Lt x y) = (frVars x) ++ (frVars y)
-frVars (Gt x y) = (frVars x)++ (frVars y)
-frVars (Eq x y) = (frVars x) ++ (frVars y)
-frVars (If x y z)= (frVars x) ++ (frVars y) ++ (frVars z)
-frVars (Let x y z) = (frVars y) ++ (frVars z)
-frVars (Fn x y) = (frVars y)
-frVars (App x y) = (frVars x) ++ (frVars y)
-frVars (Alloc x) = (frVars x)
-frVars (Dref x) = (frVars x)
-frVars (Assign x y) = (frVars x) ++ (frVars y)
-frVars Void = []
-frVars (Seq x y)= (frVars x) ++ (frVars y)
-frVars (While x y) = (frVars x) ++ (frVars y)
-frVars (Abs x a1) = filter (/= x) (frVars a1)
+--- MUL
+eval1 (E s m (Mul e1 e2)) = E (S (MulFL e2) s) m e1
+eval1 (R (S (MulFL e2) s) m v) = E (S (MulFR v) s) m e2
+eval1 (R (S (MulFR (I v1)) s) m (I v2)) = R s m (I (v1+v2))
+--- SUCC
+eval1 (E s m (Succ e)) = E (S SuccF s) m e
+eval1 (R (S SuccF s) m (I v)) = R s m (I (v+1))
+--PRED
+eval1 (E s m (Pred e))= E (S PredF s) m e
+eval1 (R (S PredF s) m (I v)) = R s m (I (1-v))
+--AND
+eval1 (E s m (And e1 e2)) = E (S (AndFL e2) s) m e1
+eval1 (R (S (AndFL e2) s) m v) = E (S (AndFR v) s) m e2
+eval1 (R (S (AndFR (B v1)) s) m (B v2)) = R s m (B (v1 && v2))
+--OR
+eval1 (E s m (Or e1 e2)) = E (S (OrFL e2) s) m e1
+eval1 (R (S (OrFL e2) s) m v) = E (S (OrFR v) s) m e2
+eval1 (R (S (OrFR (B v1)) s) m (B v2)) = R s m (B (v1 || v2))
+--- NEG
+eval1 (E s m (Not e)) = E (S NotF s) m e
+eval1 (R (S NotF s) m (B v)) = R s m (B (not v))
+--- ISZERO
+eval1 (E s m (Iszero e)) = E (S IszeroF s) m e
+eval1 (R (S IszeroF s) m (I v)) = R s m (B (0 == v))
+--LT
+eval1 (E s m (Lt e1 e2)) = E (S (LtFL e2) s) m e1
+eval1 (R (S (LtFL e2) s) m v) = E (S (LtFR v) s) m e2
+eval1 (R (S (LtFR (Var v1)) s) m (Var v2)) = R s m (Var (v2))
+--GT
+eval1 (E s m (Gt e1 e2)) = E (S (GtFL e2) s) m e1
+eval1 (R (S (GtFL e2) s) m v) = E (S (GtFR v) s) m e2
+eval1 (R (S (GtFR (Var v1)) s) m (Var v2)) = R s m (Var (v1))
+--EQ
+eval1 (E s m (Eq e1 e2)) = E (S (EqFL e2) s) m e1
+eval1 (R (S (EqFL e2) s) m v) = E (S (EqFR v) s) m e2
+eval1 (R (S (EqFR (Var v1)) s) m (Var v2)) = R s m (B (v1 == v2))
 
 
--- TODO subst. Extiende esta función para las nuevas expresiones.
-subst :: Expr -> Substitution -> Expr
-subst (Var x) (a,b) = if x == a
-                      then b
-                      else Var x
-subst (I x) _ = I x
-subst (B x) _ = B x
-subst (L x ) _ = L x
-subst (Add x y) e =  Add (subst x e) (subst y e)
-subst (Mul x y) e = Mul (subst x e) (subst y e)
-subst (Succ x) e = Succ (subst x e)
-subst (Pred x) e = Pred (subst x e)
-subst (And x y) e = And (subst x e) (subst y e)
-subst (Or x y) e = Or(subst x e) (subst y e)
-subst (Not x) e = Not(subst x e)
-subst (Iszero x) e = Iszero(subst x e)
-subst (Lt x y) e = Lt (subst x e) (subst y e)
-subst (Gt x y) e = Gt (subst x e) (subst y e)
-subst (Eq x y) e = Eq (subst x e) (subst y e)
-subst (If x y z) e = If (subst x e) (subst y e) (subst z e)
-subst (Let x y z) e = Let x (subst y e) (subst z e)
-subst (Fn x y) e = Fn x (subst y e)
-subst (App x y) e = App (subst x e) (subst y e)
-subst (Alloc x) e = Alloc (subst x e)
-subst (Dref x) e = Dref (subst x e)
-subst (Assign x y) e = Assign (subst x e) (subst y e)
-subst Void e = Void
-subst (Seq x y) e= Seq (subst x e) (subst y e)
-subst (While x y) e = While (subst x e) (subst y e)
-subst (Abs z e) s@(x,r)
-  | z == x || elem z (frVars r) = error "Se requiere una equivalencia"
-  | otherwise = Abs z (subst e s)
--}
 
-{-- TODO Extiende las funciones de eval1, evals y evale para incluir el manejo de excepciones con valor
-usando las expresiones que agregamos a la sintaxis.
 
-TEST
-eval1 P (S (HandleF "x" (V "x")) Empty) (Raise (B False))
--}
+--- WHILE
+eval1 (E s m w@(While f e)) = E s m (If f (Seq e w) Void)
+
+
