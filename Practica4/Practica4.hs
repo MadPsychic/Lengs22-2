36d0d2f92e33a44defa84ec7531d39a1c0e53568
jorgevillmal <53312702+jorgevillmal@users.noreply.github.com>
2022-06-19 17:48:06 -0500

    Update Practica4.hs

--- Practica4/Practica4.hs
+++ Practica4/Practica4.hs
@@ -92,7 +92,6 @@ eval1. Re implementa esta función para que dado un estado, devuelva un paso de 
 esdecir,eval1s=s’siysólosis→k s’
 --}
 
-eval1 :: State -> State
 --- VALUES
 eval1 (E s m e@(I n)) = R s m e
 eval1 (E s m e@(B b)) = R s m e
@@ -107,43 +106,57 @@ eval1 (R (S (AddFR (I v1)) s) m (I v2)) = R s m (I (v1+v2))
 eval1 (E s m (Mul e1 e2)) = E (S (MulFL e2) s) m e1
 eval1 (R (S (MulFL e2) s) m v) = E (S (MulFR v) s) m e2
 eval1 (R (S (MulFR (I v1)) s) m (I v2)) = R s m (I (v1+v2))
+
 --- SUCC
 eval1 (E s m (Succ e)) = E (S SuccF s) m e
 eval1 (R (S SuccF s) m (I v)) = R s m (I (v+1))
+
 --PRED
 eval1 (E s m (Pred e))= E (S PredF s) m e
 eval1 (R (S PredF s) m (I v)) = R s m (I (1-v))
+
 --AND
 eval1 (E s m (And e1 e2)) = E (S (AndFL e2) s) m e1
 eval1 (R (S (AndFL e2) s) m v) = E (S (AndFR v) s) m e2
 eval1 (R (S (AndFR (B v1)) s) m (B v2)) = R s m (B (v1 && v2))
+
 --OR
 eval1 (E s m (Or e1 e2)) = E (S (OrFL e2) s) m e1
 eval1 (R (S (OrFL e2) s) m v) = E (S (OrFR v) s) m e2
 eval1 (R (S (OrFR (B v1)) s) m (B v2)) = R s m (B (v1 || v2))
+
 --- NEG
 eval1 (E s m (Not e)) = E (S NotF s) m e
 eval1 (R (S NotF s) m (B v)) = R s m (B (not v))
+
 --- ISZERO
 eval1 (E s m (Iszero e)) = E (S IszeroF s) m e
 eval1 (R (S IszeroF s) m (I v)) = R s m (B (0 == v))
+
 --LT
 eval1 (E s m (Lt e1 e2)) = E (S (LtFL e2) s) m e1
 eval1 (R (S (LtFL e2) s) m v) = E (S (LtFR v) s) m e2
-eval1 (R (S (LtFR (Var v1)) s) m (Var v2)) = R s m (Var (v2))
+eval1 (R (S (LtFR (I v1)) s) m (I v2)) = R s m (I (v1 > v2))
+
 --GT
 eval1 (E s m (Gt e1 e2)) = E (S (GtFL e2) s) m e1
 eval1 (R (S (GtFL e2) s) m v) = E (S (GtFR v) s) m e2
 eval1 (R (S (GtFR (Var v1)) s) m (Var v2)) = R s m (Var (v1))
+
 --EQ
 eval1 (E s m (Eq e1 e2)) = E (S (EqFL e2) s) m e1
 eval1 (R (S (EqFL e2) s) m v) = E (S (EqFR v) s) m e2
 eval1 (R (S (EqFR (Var v1)) s) m (Var v2)) = R s m (B (v1 == v2))
 
+--APP
+eval1 (E s m (App e1 e2)) = E (S (AppFL e2) s) m e1
+eval1 (R (S (AppFL e2) s) m v) = E (S (AppFR v) s) m e2
+eval1 (R (S (AppFR (Var v1)) s) m (Var v2)) = R s m (Var (v2)) -- Este hay que corregirlo
 
-
+-- IF
 
 --- WHILE
 eval1 (E s m w@(While f e)) = E s m (If f (Seq e w) Void)
+
 
 
